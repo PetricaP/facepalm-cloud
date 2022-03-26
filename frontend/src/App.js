61729a716033ac1e81d10a6ffb376f90a2be3c31
Petru Petrica <petrica.petru99@gmail.com>
2022-03-26 13:54:04 +0200

    Fetched a user from backend locally and rendered on page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+
+function Interest(props) {
+  return <div className="interest">{props.value}</div>
+}
+
+
+function User(props) {
+  const interests = props.user.interests?.map((interest) => <Interest key={interest} value={interest}/>);
+
+  return (
+    <div className="user">
+      <div className="username">{props.user.username}</div>
+      <div className="content">
+        <div className="followers-container">
+          <div className="followers">{props.user.followers}</div>
+          <div className="following">{props.user.following}</div>
+        </div>
+        <p className="status">{props.user.status}</p>
+        <div className="interests-container">{interests}</div>
+        <p className="email">{props.user.email}</p>
+        <p className="address">{props.user.address}</p>
+      </div>
+    </div>
+  )
+}
+
 
 function App() {
+  const [user, setUser] = useState("User");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/users/haroldwatkins")
+    .then((response) => response.json())
+    .then((res) =>
+      setUser(res)
+    );
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <User user={user}/>
     </div>
   );
 }
